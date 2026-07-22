1dd700417872be97566d9bfd43ec5aff7a6ab97c
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-07-21 22:31:27 -0300

    Create User.java

---
+++ Backend/finance_ai/src/main/java/com/alura/finance_ai/model/User.java
@@ -0,0 +1,40 @@
+package com.alura.finance_ai.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(min = 2, max = 20, message = "El nombre debe tener entre 2 y 20 caracteres")
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede estar vacío")
+    @Size(min = 2, max = 20, message = "El apellido debe tener entre 2 y 20 caracteres")
+    @Column(nullable = false)
+    private String apellido;
+
+    @NotBlank(message = "El email no puede estar vacío")
+    @Column(nullable = false, unique = true)
+    @Email(message = "El formato del email no es válido")
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Column(nullable = false)
+    private String contrasena;
+}
