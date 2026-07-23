398c8a1f7f65b1882324a30b222f57401ebf5a79
CarlosS13 <Carloscaprari36@gmail.com>
2026-07-22 21:13:04 -0300

    Update JwtUtil.java creacion de generateToken()

--- Backend/finance_ai/src/main/java/com/alura/finance_ai/auth/security/JwtUtil.java
+++ Backend/finance_ai/src/main/java/com/alura/finance_ai/auth/security/JwtUtil.java
@@ -1,4 +1,30 @@
 package com.alura.finance_ai.auth.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    private static final String SECRET_KEY = "mi_clave_secreta_super_segura_para_finance_ai_app";
+
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 10;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
