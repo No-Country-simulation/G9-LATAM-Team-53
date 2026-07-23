692a9dd01de581a853ae87e3faecab15dccea457
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-07-22 21:40:26 -0300

    metodo existsByToken en TokenInvalidoRepository

--- Backend/finance_ai/src/main/java/com/alura/finance_ai/auth/repository/TokenInvalidoRepository.java
+++ Backend/finance_ai/src/main/java/com/alura/finance_ai/auth/repository/TokenInvalidoRepository.java
@@ -1,4 +1,12 @@
 package com.alura.finance_ai.auth.repository;
 
-public interface TokenInvalidoRepository {
+import com.alura.finance_ai.auth.model.TokenInvalido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenInvalidoRepository extends JpaRepository<TokenInvalido, Long> {
+
+    boolean existsByToken(String token);
+
 }
