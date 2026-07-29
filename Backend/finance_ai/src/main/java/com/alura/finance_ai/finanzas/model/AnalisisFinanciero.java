b0219fa78fce53cefe2a549697dbe274184fda85
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-07-29 16:26:05 -0300

    Create AnalisisFinanciero.java

---
+++ Backend/finance_ai/src/main/java/com/alura/finance_ai/finanzas/model/AnalisisFinanciero.java
@@ -0,0 +1,19 @@
+package com.alura.finance_ai.finanzas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@Table(name = "analisis_financiero")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnalisisFinanciero {
+}
