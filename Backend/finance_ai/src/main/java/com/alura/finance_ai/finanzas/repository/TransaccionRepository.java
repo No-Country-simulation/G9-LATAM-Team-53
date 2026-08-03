830181116695f5397418a8a18c5bb1f3142ad135
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-03 18:31:47 -0300

    Update TransaccionRepository.java

--- Backend/finance_ai/src/main/java/com/alura/finance_ai/finanzas/repository/TransaccionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alura.finance_ai.finanzas.repository;
-
-import com.alura.finance_ai.finanzas.model.Transaccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
-}
