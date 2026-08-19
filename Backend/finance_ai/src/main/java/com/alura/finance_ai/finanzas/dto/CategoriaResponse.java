e33a0c313acea58f484e6dc8a146a87682944218
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2026-08-19 20:41:22 -0300

    clear

--- Backend/finance_ai/src/main/java/com/alura/finance_ai/finanzas/dto/CategoriaResponse.java
+++
@@ -1,7 +0,0 @@
-package com.alura.finance_ai.finanzas.dto;
-
-public record CategoriaResponse(
-        Long id,
-        String nombre
-) {
-}
--- Backend/finance_ai/src/main/java/com/alura/finance_ai/finanzas/repository/AnalisisFinancieroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alura.finance_ai.finanzas.repository;
-
-import com.alura.finance_ai.finanzas.model.AnalisisFinanciero;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnalisisFinancieroRepository extends JpaRepository<AnalisisFinanciero, Long> {
-}
